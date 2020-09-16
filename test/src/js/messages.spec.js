d75457b9a951906b45dea3eab12ed912604e30f2
Tye, Thurston <thurston.tye@ft.com>
2020-09-16 15:33:18 +0100

    test correct messaging for rich content messages

--- test/src/js/messages.spec.js
+++ test/src/js/messages.spec.js
@@ -2,6 +2,7 @@
 'use strict';
 
 import {richContentMessage , getMessage} from '../../../src/js/messages';
+import {RICH_CONTENT_MESSAGES} from '../../../src/js/config';
 const itemsFixture = require('../../fixtures/item.json');
 const userStatusFixture = require('../../fixtures/userStatus.json');
 //const getMessageFixture = require('../../fixtures/getMessageFixture');
@@ -11,32 +12,57 @@ const userStatusFixture = require('../../fixtures/userStatus.json');
 describe('messages', () => {
 
 	describe('getMessages', () => {
-		it('to return a string', () => {
+		it('to return CONTRACTUAL_RIGHTS_CONSIDERATION message', () => {
 			const subject = getMessage(itemsFixture,userStatusFixture);
-			expect(typeof subject).toBe('string');
-			//expect(subject).to.equal(getMessageFixture);
+			expect(subject).toBe('<p>Please ensure you have considered your <a data-trackable="contractual-rights" href="/republishing/contract">contractual rights</a> before republishing.</p>');
 			//TODO test all variations of MESSAGES as defined in config based on messageCode provided
 		});
 	});
 
 	describe('richContentMessage', () => {
 		it('returns an empty string if item does not have graphics', () => {
-
-			//const subject = document.createElement('div');
 			const richContentMsg = richContentMessage(itemsFixture,userStatusFixture);
+			document.body.innerHTML = richContentMsg;
 			//subject.innerHTML = richContentMsg;
-			expect(richContentMsg).toEqual('');
-			expect(typeof richContentMsg).toBe('string');
+			expect(richContentMsg).toEqual(expect.not.stringContaining(RICH_CONTENT_MESSAGES.GRAPHICS));
+			expect(richContentMsg).toEqual(expect.not.stringContaining(RICH_CONTENT_MESSAGES.WORD_FORMAT));
 		});
 
-		it('returns an empty string if item does not have graphics', () => {
+		it('returns empty string if if hasGraphics is FALSE and canAllGraphicsBeSyndicated is TRUE', () => {
 			// eslint-disable-next-line no-undef
-			const items = Object.assign({}, itemsFixture, {hasGraphics: true});
-			//const subject = document.createElement('div');
+			const items = Object.assign({}, itemsFixture, {hasGraphics: true, canAllGraphicsBeSyndicated: true });
 			const richContentMsg = richContentMessage(items,userStatusFixture);
-			//subject.innerHTML = richContentMsg;
-			//expect(richContentMsg).to.equal(bothMessages);
 			expect(typeof richContentMsg).toBe('string');
+			expect(richContentMsg).toEqual(expect.not.stringContaining(RICH_CONTENT_MESSAGES.GRAPHICS));
+			expect(richContentMsg).toEqual(expect.not.stringContaining(RICH_CONTENT_MESSAGES.WORD_FORMAT));
+		});
+
+		it('returns empty string if if hasGraphics is FALSE and canAllGraphicsBeSyndicated is FALSE', () => {
+			// eslint-disable-next-line no-undef
+			const items = Object.assign({}, itemsFixture, {hasGraphics: true, });
+			const richContentMsg = richContentMessage(items,userStatusFixture);
+			expect(typeof richContentMsg).toBe('string');
+			expect(richContentMsg).toEqual(expect.stringContaining(RICH_CONTENT_MESSAGES.GRAPHICS));
+			expect(richContentMsg).toEqual(expect.not.stringContaining(RICH_CONTENT_MESSAGES.WORD_FORMAT));
+		});
+
+		it('returns HTML including the graphics unavailable message if hasGraphics is TRUE and canAllGraphicsBeSyndicated is FALSE', () => {
+			// eslint-disable-next-line no-undef
+			const items = Object.assign({}, itemsFixture, {hasGraphics: true, });
+			const richContentMsg = richContentMessage(items,userStatusFixture);
+			expect(typeof richContentMsg).toBe('string');
+			expect(richContentMsg).toEqual(expect.stringContaining(RICH_CONTENT_MESSAGES.GRAPHICS));
+			expect(richContentMsg).toEqual(expect.not.stringContaining(RICH_CONTENT_MESSAGES.WORD_FORMAT));
+		});
+
+		it('returns a string of HTML that contains the graphics unavailable message AND the word format message if hasGraphics is TRUE and canAllGraphicsBeSyndicated is FALSE AND download_format is docx', () => {
+			// eslint-disable-next-line no-undef
+			const items = Object.assign({}, itemsFixture, {hasGraphics: true, });
+			const userStatus = Object.assign({}, userStatusFixture, {download_format: 'docx', });
+			const richContentMsg = richContentMessage(items,userStatus);
+			expect(typeof richContentMsg).toBe('string');
+			expect(richContentMsg).toEqual(expect.stringContaining(RICH_CONTENT_MESSAGES.GRAPHICS));
+			expect(richContentMsg).toEqual(expect.stringContaining(RICH_CONTENT_MESSAGES.WORD_FORMAT));
 		});
 
 	});
