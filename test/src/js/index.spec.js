b47f8bc8d3b1dc2f2582930ca9e7c156a754e654
Tye, Thurston <thurston.tye@ft.com>
2020-10-19 20:32:15 +0100

    testcover for every init condition

--- test/src/js/index.spec.js
+++ test/src/js/index.spec.js
@@ -33,7 +33,7 @@ describe('#init should return undefined and ', function () {
 		getUserStatus.mockReset();
 	});
 
-	test('should NOT initiate any syndication features if the syndication flag is falsy', async () => {
+	test('should NOT call getSyndicationAccess or initialise any syndication features if the syndication flag is falsy', async () => {
 		const flagMock = {
 			get: () => {
 				return false;
@@ -50,7 +50,7 @@ describe('#init should return undefined and ', function () {
 		expect(initNavigation).not.toHaveBeenCalled();
 	});
 
-	test('should not attempt to get user status or initialise syndication when not syndication customer', async () => {
+	test('should NOT call getUserStatus or initialise syndication features when no syndication product code is present', async () => {
 		const flagMock = {
 			get: () => true,
 		};
@@ -62,13 +62,14 @@ describe('#init should return undefined and ', function () {
 
 		expect(subject).toBe(undefined);
 
+		expect(getUserStatus).not.toHaveBeenCalled();
 		expect(initDataStore).not.toHaveBeenCalled();
 		expect(initIconify).not.toHaveBeenCalled();
 		expect(initDownloadModal).not.toHaveBeenCalled();
 		expect(initNavigation).not.toHaveBeenCalled();
 	});
 
-	test('should return undefined if the user isn’t migrated and NOT initiate any syndication features ', async () => {
+	test('should NOT initialise any syndication features if user migrated status is FALSE ', async () => {
 		const flagMock = {
 			get: () => true,
 		};
@@ -89,10 +90,26 @@ describe('#init should return undefined and ', function () {
 		expect(initNavigation).not.toHaveBeenCalled();
 	});
 
-	// 	test.skip('should initialise the republising navigation', () => { });
+	test('should NOT initialise any syndication features if user has S2 code but not S1 ', async () => {
+		const flagMock = {
+			get: () => true,
+		};
+
+
+		getSyndicationAccess.mockResolvedValue(['S2', 'P1']);
+		getUserStatus.mockResolvedValue(userFixture);
+
+		const subject = await init(flagMock);
+
+		expect(subject).toBe(undefined);
+		expect(initDataStore).not.toHaveBeenCalled();
+		expect(initIconify).not.toHaveBeenCalled();
+		expect(initDownloadModal).not.toHaveBeenCalled();
+		expect(initNavigation).not.toHaveBeenCalled();
+	});
 
 	//Happy path
-	test('should initialise syndication features the user has republishing product code S1', async () => {
+	test('should initialise syndication features if the user has republishing product code S1', async () => {
 		const flagMock = {
 			get: () => true,
 		};
@@ -109,20 +126,43 @@ describe('#init should return undefined and ', function () {
 		expect(initNavigation).toHaveBeenCalledWith(userFixture);
 	});
 
+	test('should initialise syndication navigation only and NOT other features if Spanish content is allowed but NOT ft.com', async () => {
+		const flagMock = {
+			get: () => true,
+		};
+
+		const spanishContentOnlyUser = JSON.parse(JSON.stringify(userFixture));
+		spanishContentOnlyUser.allowed.ft_com = false;
+
+		getSyndicationAccess.mockResolvedValue(['S1', 'P1']);
+		getUserStatus.mockResolvedValue(spanishContentOnlyUser);
+
+		const subject = await init(flagMock);
+
+		expect(subject).toBe(undefined);
+
+		expect(initNavigation).toHaveBeenCalledWith(spanishContentOnlyUser);
+		expect(initDataStore).not.toHaveBeenCalled();
+		expect(initIconify).not.toHaveBeenCalled();
+		expect(initDownloadModal).not.toHaveBeenCalled();
+	});
+
 	test('should initialise syndication features including rich article access the user has republishing product code S1 and S2', async () => {
 		const flagMock = {
 			get: () => true,
 		};
 
+		const user = userFixture;
 		getSyndicationAccess.mockResolvedValue(['S1', 'S2', 'P1']);
 		getUserStatus.mockResolvedValue(userFixture);
 
 		const subject = await init(flagMock);
 
 		expect(subject).toBe(undefined);
-		expect(initDataStore).toHaveBeenCalledWith(userFixture);
-		expect(initIconify).toHaveBeenCalledWith(userFixture);
-		expect(initDownloadModal).toHaveBeenCalledWith(userFixture);
-		expect(initNavigation).toHaveBeenCalledWith(userFixture);
+		expect(userFixture.allowed.rich_article).toBe(true);
+		expect(initDataStore).toHaveBeenCalledWith(user);
+		expect(initIconify).toHaveBeenCalledWith(user);
+		expect(initDownloadModal).toHaveBeenCalledWith(user);
+		expect(initNavigation).toHaveBeenCalledWith(user);
 	});
 });
